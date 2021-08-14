85e592cd47636810f8635b8bf0cc5989667ddf02
hallday0024 <hallgg0024@gmail.com>
2021-08-14 15:37:28 +0700

    Hall

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,14 +4,45 @@ import { Link } from "gatsby";
 
 // Step 2: Define your component
 const AboutPage = () => {
+  let A = [
+    [1, -1, 3],
+    [2, 0, 1],
+    [0, 2, -5],
+  ];
+  let B = [
+    [1, 0, 1],
+    [-1, 1, 3],
+    [2, 4, 0],
+  ];
+
+  function matrix(A, B) {
+    return A.map((row, i) =>
+      B[0].map((_, j) => A[i].reduce((sum, _, n) => sum + A[i][n] * B[n][j], 0))
+    );
+  }
+
   return (
     <main>
       <title>About Me</title>
-      <h1>About Me</h1>
+      <link
+        rel="icon"
+        href="https://image.flaticon.com/icons/png/512/5256/5256145.png"
+      ></link>
+      <h1>matrix multipliy</h1>
       <Link to="/">Home</Link>
-      <p>
-        Hi there! I'm the proud creator of this site, which I built with Gatsby.
-      </p>
+      <div className="container">
+        <table>
+          <tbody>
+            {matrix(A, B).map((row) => (
+              <tr key={row.id}>
+                {row.map((col) => (
+                  <td key={col.id}>{col}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 };
