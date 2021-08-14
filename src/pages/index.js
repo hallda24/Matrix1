49f02aef4fe2ba67baeb92cd43d4b36abc8eb6ca
hallday0024 <hallgg0024@gmail.com>
2021-08-14 12:30:32 +0700

    Hall

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,9 @@ import { Link } from "gatsby";
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title>
+      <title>Deploy React</title>
       <h1>Welcome to my Gatsby site!</h1>
-      <Link to="../pages/about.js">About</Link>
+      <Link to="/about.js">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   );
